f665c813e265dbcd85da8916e80b145a65ad0018
Murray Logan <i.obesulus@gmail.com>
2024-10-07 16:12:17 +1000

    typo

--- R/config.R
+++ R/config.R
@@ -36,7 +36,7 @@ config <- function(){
 
     reefCloudPackage::change_status(stage = "STAGE1", item = "Prepare file system", status = "success",
                              update_display = FALSE)
-    status::add_status_item(stage = 1, item = "Prepare_system", name = "prepare_file_system", status = "success")
+    status::add_status_item(stage = 1, item = "prepare_file_system", name = "Prepare file system", status = "success")
 
   }, LOG_FILE, '--Config--', msg='configure necessary folders', return=NULL,
   stage = NULL, item = NULL)
