4c6cfeeffe9d53efafbc190dfef6b5a6e9253c23
Murray Logan <m.logan@aims.gov.au>
2025-07-28 05:07:08 +0000

    fixing bugs with geoserver

--- R/get_geoserver_data.R
+++ R/get_geoserver_data.R
@@ -4,11 +4,11 @@
 #' @param Tier An integer indicating the tier of the data to be retrieved.
 #' @param cov_name A string indicating the name of the coverage to be retrieved.
 #' @return A spatial data frame containing the filtered data from the geoserver.
-#' @details Requires BY_TIER, DATA_PATH, and geo_info to be available in the environment. 
+#' @details Requires BY_TIER, DATA_PATH, and rc_client to be available in the environment. 
 #' @author Murray Logan
 #' @examples
 #' get_geoserver_info()
-#' cov_data <- get_geoserver_data(Tier = 4, cov_name = "degrees_heating_weeks_tier")
+#' cov_data <- get_geoserver_data(Tier = 4, cov_name = "degrees_heating_weeks_tier", rc_client)
 #' @export
 get_geoserver_data <- function(Tier = as.numeric(BY_TIER) - 1, cov_name = NULL, rc_client) {
   status::status_try_catch(
@@ -27,39 +27,6 @@ get_geoserver_data <- function(Tier = as.numeric(BY_TIER) - 1, cov_name = NULL, 
     cov_data <-  rc_client$getFeatures(cov_name, srsName = "EPSG:4326", bbox= bbox)
     })
     )
-    # wch <- str_which(geo_info$rc_lyrs, cov_name)
-    
-    # wch_tier_id <- tier.sf %>%
-    #   pull(tier_id) %>%
-    #   unique()
-    # bbox <- st_bbox(tier.sf) %>% 
-    #   as.character() %>%
-    #   paste(.,collapse = ',')
-
-      
-    # url <- geo_info$url
-    # url$query <- list(service = "WFS",
-    #   version = "1.0.0",
-    #   request = "GetFeature",
-    #   typename = geo_info$rc_lyrs[wch], 
-    #   bbox = bbox,
-    #   srs="EPSG%3A4326",
-    #   styles='',
-    #   format="application/openlayers")
-
-    # request <- build_url(url)
-    # temp_file <- tempfile()
-    # err <- try(download.file(request, temp_file, quiet = TRUE))
-    # if (inherits(err, "try-error")) {
-    #   cov_data <- NULL
-    #   stop(err)
-    # } else {
-    #   cov_data <- read_sf(temp_file) %>%
-    #     st_set_crs(4326) %>%
-    #     filter(tier == Tier, tier_id %in% wch_tier_id) %>%
-    #     suppressWarnings() %>%
-    #     suppressMessages()
-    # }
   },
   stage_ = 2,
   order_ = 10,
--- R/get_geoserver_info.R
+++ R/get_geoserver_info.R
@@ -6,8 +6,7 @@
 #' @export
 #' @examples
 #' get_geoserver_info()
-#' # This will load `geo_info` in the global environment 
-#' # with `rc_lyrs` (available layers) and `url` (parsed URL).
+#' # This will load `rc_client` in the global environment 
 get_geoserver_info <- function() {
   status::status_try_catch(
   {
@@ -22,18 +21,7 @@ get_geoserver_info <- function() {
      logger = "INFO",
      headers = c("User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
     )
-    # rc_geo<-"https://geoserver.apps.aims.gov.au/reefcloud/ows"
-    # rc_client <- WFSClient$new(rc_geo, 
-    #                           serviceVersion = "2.0.0",
-    #                           logger = "DEBUG",
-    #                           headers = c("User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"))
-    # rc_client$getFeatureTypes(pretty = TRUE)
-    # rc_lyrs<-rc_client$getFeatureTypes() %>%
-    #   map_chr(function(x){x$getName()})
-    # url <- parse_url(rc_geo)
-    # geo_info <- list(rc_lyrs = rc_lyrs, url = url)
     assign("rc_client", rc_client, env =  .GlobalEnv)
-  #  assign("geo_info", geo_info, env =  .GlobalEnv)
   },
   stage_ = 2,
   order_ = 9,
