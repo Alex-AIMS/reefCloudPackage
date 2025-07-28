6efe65c7402b292475113a6ffc6280554982bd7a
Murray Logan <m.logan@aims.gov.au>
2025-07-28 04:57:33 +0000

    fix bugs with geoserver

--- R/get_geoserver_data.R
+++ R/get_geoserver_data.R
@@ -10,42 +10,56 @@
 #' get_geoserver_info()
 #' cov_data <- get_geoserver_data(Tier = 4, cov_name = "degrees_heating_weeks_tier")
 #' @export
-get_geoserver_data <- function(Tier = as.numeric(BY_TIER) - 1, cov_name = NULL) {
+get_geoserver_data <- function(Tier = as.numeric(BY_TIER) - 1, cov_name = NULL, rc_client) {
   status::status_try_catch(
   {
     load(file=paste0(DATA_PATH,'primary/tier', as.numeric(BY_TIER) - 1, '.sf.RData'))
-    
-    wch <- str_which(geo_info$rc_lyrs, cov_name)
-    
+
     wch_tier_id <- tier.sf %>%
       pull(tier_id) %>%
       unique()
     bbox <- st_bbox(tier.sf) %>% 
       as.character() %>%
       paste(.,collapse = ',')
-    url <- geo_info$url
-    url$query <- list(service = "WFS",
-      version = "1.0.0",
-      request = "GetFeature",
-      typename = geo_info$rc_lyrs[wch], 
-      bbox = bbox,
-      srs="EPSG%3A4326",
-      styles='',
-      format="application/openlayers")
 
-    request <- build_url(url)
-    temp_file <- tempfile()
-    err <- try(download.file(request, temp_file, quiet = TRUE))
-    if (inherits(err, "try-error")) {
-      cov_data <- NULL
-      stop(err)
-    } else {
-      cov_data <- read_sf(temp_file) %>%
-        st_set_crs(4326) %>%
-        filter(tier == Tier, tier_id %in% wch_tier_id) %>%
-        suppressWarnings() %>%
-        suppressMessages()
-    }
+    invisible(
+    capture.output({
+    cov_data <-  rc_client$getFeatures(cov_name, srsName = "EPSG:4326", bbox= bbox)
+    })
+    )
+    # wch <- str_which(geo_info$rc_lyrs, cov_name)
+    
+    # wch_tier_id <- tier.sf %>%
+    #   pull(tier_id) %>%
+    #   unique()
+    # bbox <- st_bbox(tier.sf) %>% 
+    #   as.character() %>%
+    #   paste(.,collapse = ',')
+
+      
+    # url <- geo_info$url
+    # url$query <- list(service = "WFS",
+    #   version = "1.0.0",
+    #   request = "GetFeature",
+    #   typename = geo_info$rc_lyrs[wch], 
+    #   bbox = bbox,
+    #   srs="EPSG%3A4326",
+    #   styles='',
+    #   format="application/openlayers")
+
+    # request <- build_url(url)
+    # temp_file <- tempfile()
+    # err <- try(download.file(request, temp_file, quiet = TRUE))
+    # if (inherits(err, "try-error")) {
+    #   cov_data <- NULL
+    #   stop(err)
+    # } else {
+    #   cov_data <- read_sf(temp_file) %>%
+    #     st_set_crs(4326) %>%
+    #     filter(tier == Tier, tier_id %in% wch_tier_id) %>%
+    #     suppressWarnings() %>%
+    #     suppressMessages()
+    # }
   },
   stage_ = 2,
   order_ = 10,
